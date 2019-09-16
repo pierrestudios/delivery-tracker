8642e05c4a6203a2db259ddb72b99300579c8633
pierrestudios <info@pierrestudios.com>
2019-09-16 15:02:06 -0400

    Rework App test

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import { shallow } from "enzyme";
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+import App from "./App";
+import Store from "./store";
+
+it("renders without crashing", () => {
+  shallow(
+    <Store>
+      <App />
+    </Store>
+  );
 });
