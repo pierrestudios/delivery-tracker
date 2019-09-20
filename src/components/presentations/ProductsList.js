c0339f5e89928d08e2cae22ae3fb706a05bba8b7
pierrestudios <info@pierrestudios.com>
2019-09-20 10:15:23 -0400

    Remove filtering from ProductsList

--- src/components/presentations/ProductsList.js
+++ src/components/presentations/ProductsList.js
@@ -2,8 +2,8 @@ import React from "react";
 
 import ProductRow from "./ProductRow";
 
-export default ({ data, category, viewDetails }) => {
-  return data
-    .filter(p => category === p.category)
-    .map(d => <ProductRow {...d} viewDetails={viewDetails} key={d.id} />);
+export default ({ data, viewDetails }) => {
+  return data.map(d => (
+    <ProductRow {...d} viewDetails={viewDetails} key={d.id} />
+  ));
 };
